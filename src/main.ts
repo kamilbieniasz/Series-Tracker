30115341e480eeb25babbae37d261d6ea9823bfc
Kamil <kamilbieniasz98@gmail.com>
2022-05-11 18:03:18 +0200

    Added primeVue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import PrimeVue from 'primevue/config';
 import {FontAwesomeIcon} from "./plugins/font-awesome"
 
 createApp(App)
     .use(router)
+    .use(PrimeVue, {ripple: true})
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
